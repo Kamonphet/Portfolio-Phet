5774e43d1974bf924449d70eda1af9fc2c677878
Phet Kamonphet ✅ <kamonpach.siri@gmail.com>
2024-04-11 17:54:52 +0700

    sidebar commit

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
 import "./Sidebar.css"
+import Logo from "../../assets/logo.svg"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className='aside'>
+      <a href="#home" className='nav__logo'>
+        <img src={Logo} alt="" />
+      </a>
+
+      <nav className='nav'>
+        <div className='nav__menu'>
+          <ul className='nav__list'>
+            <li className='nav__item'>
+              <a href="#home" className='nav__link'>
+                <i class='bx bx-home-alt-2' style={{color: "black",fontSize:"20px"}}></i>
+              </a>
+            </li>
+
+            <li className='nav__item'>
+              <a href="#about" className='nav__link'>
+                <i class='bx bx-info-circle' style={{color: "black",fontSize:"20px"}}></i>
+              </a>
+            </li>
+
+            <li className='nav__item'>
+              <a href="#service" className='nav__link'>
+              <i class='bx bx-briefcase' style={{color: "black",fontSize:"20px"}}></i>
+              </a>
+            </li>
+
+            <li className='nav__item'>
+              <a href="#resume" className='nav__link'>
+                <i class='bx bxs-graduation' style={{color: "black",fontSize:"20px"}}></i>
+              </a>
+            </li>
+
+            <li className='nav__item'>
+              <a href="#portfolio" className='nav__link'>
+                <i class='bx bx-book' style={{color: "black",fontSize:"20px"}}></i>
+              </a>
+            </li>
+
+            <li className='nav__item'>
+              <a href="#testimonials" className='nav__link'>
+                <i class='bx bx-file-blank' style={{color: "black",fontSize:"20px"}}></i>
+              </a>
+            </li>
+
+            <li className='nav__item'>
+              <a href="#blog" className='nav__link'>
+                <i class='bx bx-edit' style={{color: "black",fontSize:"20px"}}></i>
+              </a>
+            </li>
+
+            <li className='nav__item'>
+              <a href="#contact" className='nav__link'>
+                <i class='bx bx-phone' style={{color: "black",fontSize:"20px"}}></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className='nav__footer'>
+        <span className='copyright'>&#169; 2022 -2023.</span>
+      </div>
+    </aside>
   )
 }
 
